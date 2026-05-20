6efc65f68452cf605798f622dd621d49838607c5
Rajesh Varikuntla <raj333921@gmail.com>
2026-05-20 20:22:41 +0200

    Fix i18n initialization - import config first before React

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './i18n/config'; // MUST be first
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './i18n/config'; // Add this line
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
