ffb519849f0d9f408ba3b0f36cf817c96d533cda
vrkgandla <vrkgandla@gmail.com>
2026-05-29 10:55:53 +0200

    added sending user from company

--- src/resources/tokens/TokenList.jsx
+++ src/resources/tokens/TokenList.jsx
@@ -12,7 +12,7 @@ const TokenList = () => {
         <TextField source="code" label={t('tokens.test_code')} />
         <TextField source="is_used" label={t('tokens.used')} />
       <TextField source="isActive" label={t('tokens.active')}  />
-      <TextField source="createdBy" label={t('tokens.createdby')}  />
+      <TextField source="createdby" label={t('tokens.createdby')}  />
         <DateField source="created_at" label={t('tokens.created_at')} showTime />
         <EditButton label={t('actions.edit')} />
         <DeleteButton label={t('actions.delete')} />
